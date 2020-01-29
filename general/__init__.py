f91485dd0a9dec4b7d8e3ffd92ddfdc4de178b26
Yang Lin <aofnev@yangdembp.lan>
2020-01-29 21:32:39 +0000

    functioning properly

--- general/__init__.py
+++
@@ -1,3 +0,0 @@
-from oalvays.general.email import email_session
-
-__all__ = ['email_session']
--- kaggle/__init__.py
+++
@@ -1,4 +0,0 @@
-from reduce_mem_usage import reduce_mem_usage_sd
-
-__all__ = ['reduce_mem_usage_sd']
-
